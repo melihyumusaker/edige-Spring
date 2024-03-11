4ae25049a65672fe3387389e9ae82998c0c8d938
melihyumusaker <74679907+melihyumusaker@users.noreply.github.com>
2024-03-12 00:39:03 +0300

    push

--- src/main/java/com/alibou/alibou/Controller/TrialExamController.java
+++ src/main/java/com/alibou/alibou/Controller/TrialExamController.java
@@ -59,7 +59,6 @@ public class TrialExamController {
     public ResponseEntity<?> getStudentTrialExamsByTeacher(@RequestBody GetStudentTrialExamsByTeacherDTO request){
         try {
             List<GetStudentTrialExamsDTO> studentTrialExams = trialExamService.getAllTrialExamsByStudentId(request.getStudent_id());
-
             return new ResponseEntity<>(studentTrialExams, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>("An error occurred while processing your request: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
