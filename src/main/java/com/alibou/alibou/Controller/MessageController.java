2f9673ed8c332ce88cc681cfba553a8e9169c2c1
melihyumusaker <74679907+melihyumusaker@users.noreply.github.com>
2024-03-14 01:59:10 +0300

    push

--- src/main/java/com/alibou/alibou/Controller/MessageController.java
+++ src/main/java/com/alibou/alibou/Controller/MessageController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -48,6 +49,8 @@ public class MessageController {
             }
 
             List<Message> messageHistory = messageService.getMessageHistory(senderId, receiverId, page, size);
+
+            Collections.reverse(messageHistory);
 
             return ResponseEntity.ok(messageHistory);
         } catch (Exception e) {
